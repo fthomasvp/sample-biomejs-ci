e4f9514d344369e8286b46052633ea27ab11efdf
fthomasvp <fellipecab@gmail.com>
2025-07-06 01:20:42 +0100

    feat: add simple project structure
    - Add Biome.js and TypeScript dependencies.
    
    - Add VSCode settings and extension recommendations.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { add, subtract } from "./utils/math";
+
+const result = add(2, 1);
+console.log("> 2 + 1 =", result);
+
+const result2 = subtract(2, 1);
+console.log("> 2 - 1 =", result2);
---
+++ src/utils/math.ts
@@ -0,0 +1,13 @@
+export function add(num1: number, num2: number) {
+	return num1 + num2;
+}
+
+/**
+ * Some comments for the subtract function
+ * @param num1
+ * @param num2
+ * @returns
+ */
+export function subtract(num1: number, num2: number) {
+	return num1 - num2;
+}
