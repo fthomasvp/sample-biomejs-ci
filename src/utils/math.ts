68c8a0ee2063e2303b91383a06cf1f8639964cbc
fthomasvp <fellipecab@gmail.com>
2025-07-22 23:46:50 +0100

    WIP: add minor improvements

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,9 +4,6 @@ export function add(num1: number, num2: number) {
 
 /**
  * Some comments for the subtract function
- * @param num1
- * @param num2
- * @returns
  */
 export function subtract(num1: number, num2: number) {
 	return num1 - num2;
