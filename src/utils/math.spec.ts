0c09c649ed69af6bfb4527975775a8b07b3571bc
Fellipe Pires <fellipecab@gmail.com>
2025-07-27 00:22:52 +0100

    refactor: improve tests and type-check command (#11)
    * refactor: improve tests and type-check command
    
    * ci: improve pipeline
    
    - Only run the Build command when merging to the main branch.
    
    * fix: error on the workflow yaml file
    
    - When there is only the GitHub Actions workflow yaml file staged, the Biome throws an error because the path to the file was provided, but it was ignored.

--- src/utils/math.spec.ts
+++ src/utils/math.spec.ts
@@ -8,11 +8,23 @@ describe("Math utils", () => {
 	it("should sum two decimal numbers", () => {
 		expect(add(1.51, 1.61)).toBe(3.12);
 	});
+	it("should throw error if numbers are not numbers", () => {
+		// @ts-expect-error
+		expect(() => add(1.51, "1.61")).toThrowError("Invalid Number");
+		// @ts-expect-error
+		expect(() => add("1.61", 1.51)).toThrowError("Invalid Number");
+	});
 
 	it("should subtract two numbers", () => {
 		expect(subtract(4, 2)).toBe(2);
 	});
 	it("should subtract two numbers and output a negative value", () => {
 		expect(subtract(2, 4)).toBe(-2);
+	});
+	it("should throw error if numbers are not numbers", () => {
+		// @ts-expect-error
+		expect(() => subtract(4, "2")).toThrowError("Invalid Number");
+		// @ts-expect-error
+		expect(() => subtract("2", 4)).toThrowError("Invalid Number");
 	});
 });
