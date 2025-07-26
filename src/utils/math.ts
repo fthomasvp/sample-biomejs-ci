3186de2b5852270d5f02bd704fd0edc83e3b74c0
Fellipe Pires <fellipecab@gmail.com>
2025-07-26 23:14:48 +0100

    Feature/git hook (#10)
    * feat: add new lint command
    
    - This will only check for staged files.
    
    - Used by the git pre-commit hook.
    
    * feat: add .githooks folder
    
    - Add npm script to change git hooks directory to the `.githooks` folder after install dependencies.
    
    - Filename inside `.githooks` folder must match the ones in the `.git/hooks` folder.

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,9 +1,12 @@
+/**
+ * Receive two numbers and perform a sum operation.
+ */
 export function add(num1: number, num2: number) {
 	return num1 + num2;
 }
 
 /**
- * Some comments for the subtract function
+ * Receive two numbers and perform a subtract operation.
  */
 export function subtract(num1: number, num2: number) {
 	return num1 - num2;
