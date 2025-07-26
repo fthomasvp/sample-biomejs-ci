1e6a04eee0c8f41adf842df39e748b3914e798a3
fthomasvp <fellipecab@gmail.com>
2025-07-27 00:12:04 +0100

    refactor: improve tests and type-check command

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -2,6 +2,10 @@
  * Receive two numbers and perform a sum operation.
  */
 export function add(num1: number, num2: number) {
+	if (typeof num1 !== "number" || typeof num2 !== "number") {
+		throw Error("Invalid Number");
+	}
+
 	return num1 + num2;
 }
 
@@ -9,5 +13,9 @@ export function add(num1: number, num2: number) {
  * Receive two numbers and perform a subtract operation.
  */
 export function subtract(num1: number, num2: number) {
+	if (typeof num1 !== "number" || typeof num2 !== "number") {
+		throw Error("Invalid Number");
+	}
+
 	return num1 - num2;
 }
